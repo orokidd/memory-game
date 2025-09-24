eeb3830c401bfd6011fdec3f877b7f9c842ec05d
orokidd <gregomily@gmail.com>
2025-09-24 16:21:54 +0800

    move shuffle to util folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,6 @@
 import { useState, useEffect } from "react";
 import { Card } from "./components/Card";
-
-// Utility to shuffle array
-function shuffle(array) {
-  const result = [...array]; // copy to avoid mutating original
-  for (let i = result.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [result[i], result[j]] = [result[j], result[i]]; // swap
-  }
-  return result;
-}
-
+import { shuffle } from "./util/shuffle";
 
 export default function App() {
   const [cards, setCards] = useState([]);
