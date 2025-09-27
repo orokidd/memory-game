875b3be0d0219616c02fbe78db00df88d390f0f7
orokidd <gregomily@gmail.com>
2025-09-27 15:11:27 +0800

    back button styling

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,7 +1,11 @@
+import backIcon from "../assets/back-svg.svg";
+
 export function Options({ resetGame }) {
   return (
     <div className="options">
-      <button onClick={resetGame}>New Game</button>
+      <button onClick={resetGame}>
+        <img src={backIcon} className="back-button" alt="Back" style={{ width: "24px", height: "24px" }} />
+      </button>
     </div>
   );
 }
